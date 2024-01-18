9b2e6da5e805f874ee8311528b4e61f2e92a14de
clarktms <clarktms@gmail.com>
2024-01-17 20:38:19 -0600

    end of day 01172024

---
+++ experiments/experiments.js
@@ -0,0 +1,20 @@
+console.log("test");
+
+const monthlyRent = 500;
+
+const yearlyRent = monthlyRent * 12;
+
+console.log(yearlyRent);
+
+const myName = "Bob Frank";
+
+const fName = "Bob";
+const lName = "Frank";
+
+const sentance = `Hello ${fName} ${lName} how are you?`;
+
+console.log(sentance);
+
+const isTheSkyBlue = true;
+
+console.log(isTheSkyBlue);
