5940e3d0256c8c71583b9402e7915e9cbb16e5ad
clarktms <clarktms@gmail.com>
2024-04-03 07:54:06 -0500

    sdf

---
+++ calc/end/calculator.js
@@ -0,0 +1,96 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator = null;
+
+const screen = document.querySelector(".screen");
+
+function buttonClick(value) {
+  if (isNaN(value)) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+  screen.innerText = buffer;
+}
+
+function handleMath(symbol) {
+  if (buffer === "0") {
+    // do nothing
+    return;
+  }
+
+  const intBuffer = parseInt(buffer);
+
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+
+  previousOperator = symbol;
+
+  buffer = "0";
+}
+
+function flushOperation(intBuffer) {
+  if (previousOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (previousOperator === "\u{2212}") {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === "\u{00D7}") {
+    runningTotal *= intBuffer;
+  } else {
+    runningTotal /= intBuffer;
+  }
+}
+
+function handleSymbol(symbol) {
+  switch (symbol) {
+    case "C":
+      buffer = "0";
+      runningTotal = 0;
+      break;
+    case "=":
+      if (previousOperator === null) {
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = runningTotal;
+      runningTotal = 0;
+      break;
+    case "←":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    case "+":
+    case "\u{2212}":
+    case "\u{00D7}":
+    case "\u{00F7}":
+      handleMath(symbol);
+      break;
+    default:
+      console.log("something wrong in switch");
+  }
+}
+
+function handleNumber(numberString) {
+  if (buffer === "0") {
+    buffer = numberString;
+  } else {
+    buffer += numberString;
+  }
+}
+
+function init() {
+  document
+    .querySelector(".calc-buttons")
+    .addEventListener("click", function (event) {
+      buttonClick(event.target.innerText);
+    });
+}
+
+init();
--- experiments/experiments.js
+++ experiments/experiments.js
@@ -18,3 +18,87 @@ console.log(sentance);
 const isTheSkyBlue = true;
 
 console.log(isTheSkyBlue);
+
+function findLargestNumber(list) {
+  // return the largest number in array
+  list.sort(function (a, b) {
+    return b - a;
+  });
+
+  return list[0];
+}
+
+console.log(findLargestNumber([0, 3, 6, 2]));
+
+function makeDogObject() {
+  // return a dog object that
+  //   has a speak method that returns woof
+  //   has a name property of "Fido"
+  //   has a color property of "white"
+  //   has an age property of 6
+  const dog = {
+    name: "Fido",
+    color: "white",
+    age: 6,
+    speak() {
+      console.log("woof");
+    },
+  };
+}
+
+console.log(makeDogObject());
+
+const dog = {
+  name: "Fido",
+  color: "white",
+  age: 6,
+  speak() {
+    console.log("woof");
+  },
+};
+const dog2 = {
+  name: "Frank",
+  color: "white",
+  age: 6,
+  speak() {
+    console.log("woof");
+  },
+};
+
+const ar = [dog, dog2];
+
+const nameList = [];
+
+ar.forEach((element) => {
+  nameList.push(element.name);
+});
+
+console.log(nameList);
+
+const inventory = [
+  { name: "asparagus", type: "vegetables", quantity: 5 },
+  { name: "bananas", type: "fruit", quantity: 0 },
+  { name: "goat", type: "meat", quantity: 23 },
+  { name: "cherries", type: "fruit", quantity: 5 },
+  { name: "fish", type: "meat", quantity: 22 },
+];
+
+const tracker = {
+  runningTotal: 0,
+  items: 0,
+};
+inventory.forEach((element) => {
+  tracker.runningTotal += element.quantity;
+  tracker.items += 1;
+});
+
+console.log(tracker);
+console.log(tracker.runningTotal / tracker.items);
+
+const typeList = [];
+
+inventory.forEach((element) => {
+  if (element.type.includes("it")) typeList.push(element.name);
+});
+
+console.log(typeList);
